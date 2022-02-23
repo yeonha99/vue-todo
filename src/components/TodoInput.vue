cf2907621c63ba33cbcbbca1a1d43b1cf3cdc016
yeonha99 <rhdqn1207@naver.com>
2022-02-23 19:45:27 +0900

    feat:데이터 출력시키기

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -32,6 +32,8 @@ export default {
         clearInput(){
             this.newTodoItme='';
         }
+        ,
+
     }
 }
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        footer
-    </div>
+    <section>
+        <ul>
+            <li v-for="todoItem in todoItems" :key="todoItem">{{todoItem}}</li>
+        </ul>
+    </section>
 </template>
 
 <script>
 export default {
-    
+        data() {
+    return {
+      todoItems: []
+    }
+  },
+      created(){
+      if(localStorage.length>0){
+        for(var i=0;i<localStorage.length;i++){
+          this.todoItems.push(localStorage.key(i));
+          console.log(i)
+        }
+      }
+    }
 }
 </script>
 
