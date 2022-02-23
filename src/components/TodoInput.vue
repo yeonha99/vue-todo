003d1552047d456a45c32b25c1e2f090f6afdb41
yeonha99 <rhdqn1207@naver.com>
2022-02-23 18:51:36 +0900

    feat:버튼 클릭하고 추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-        input
+        <input type="text" v-model="newTodoItmes">
+        <button v-on:click="addTodo">추가</button>
+        
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            newTodoItme:' '
+        }
+    },
+    methods: {
+        addTodo(){
+            console.log(this.newTodoItmes);
+        }
+    },
 }
 </script>
 
